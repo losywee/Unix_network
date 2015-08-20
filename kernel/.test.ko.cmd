8f6a01ea644c5f74aa7c87631df27f9231f2d8ef
anchowee <529238084@qq.com>
2015-08-20 09:13:45 +0800

    kernel

---
+++ kernel/.test.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/anchowee/c/unix_network/kernel/test.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/anchowee/c/unix_network/kernel/test.ko /home/anchowee/c/unix_network/kernel/test.o /home/anchowee/c/unix_network/kernel/test.mod.o
---
+++ kernel/.tmp_versions/test.mod
@@ -0,0 +1,2 @@
+/home/anchowee/c/unix_network/kernel/test.ko
+/home/anchowee/c/unix_network/kernel/test.o
